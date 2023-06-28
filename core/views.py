f46e384554352bd5fe252de26e5a37794cf60912
carloslionelxd <carlos.lionel.barboza@gmail.com>
2023-06-28 15:24:26 -0300

    Cargamos proyecto con app core y base.html

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def indexView(request):
+    return render(request, 'index.html', {})
+    
+def publicacionesView(request):
+    return render(request, 'publicaciones.html', {})
