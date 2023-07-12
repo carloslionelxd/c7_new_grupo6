8e289d43f535ad875723e7c3b7ba9a43b29a8709
Alan00Aquino <133703934+Alan00Aquino@users.noreply.github.com>
2023-07-12 08:19:11 -0300

    Creé en publi. el models categorías

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+'''    Categoría:
+nombre: texto de 200 caracteres, único.
+activo: booleano, por defecto True.
+creacion: fecha y hora automática de creación.
+actualizacion: fecha y hora automática de actualización.
+El modelo Categoria debe ordenarse por el campo nombre.
+Sobre escribir el método __str__ para que retorne el campo nombre.'''
+
+class Categorias(models.Model):
+    nombre = models.CharField(max_length = 200)
+    activo = models.BooleanField(default = True)
+    creado = models.DateField(auto_now_add = True)
+    actualizado = models.DateField(auto_now_add = True)
+    def __str__(self):
+        return self.nombre
+    
+
+
+
+
+
+
